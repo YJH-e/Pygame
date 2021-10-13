bf50eac9c55a053512e69f41cd7f5e7b12243a69
YJH-e <85558095+YJH-e@users.noreply.github.com>
2021-10-13 22:21:57 +0100

    extension on house of the rising sun
    unfinished

--- 2-2.Mansion of the Day.py
+++ 2-2.Mansion of the Day.py
@@ -9,6 +9,9 @@ BLUE = (50,50,255)
 YELLOW = (255,255,0) 
 ORANGE = (255,165,0)
 LIGHTBLUE = (173,216,230)
+DARKBLUE = (7,16,142)
+OAK = (187,129,65)
+MOONWHITE = (244,246,240)
 # -- Initialise PyGame
 pygame.init() 
 # -- Blank Screen
@@ -23,8 +26,10 @@ pygame.display.set_caption("My Window")
 done = False
 
 #introduce some game logic
-sun_x = -200
-sun_y = 0
+sun_x = 0
+sun_y = 100
+moon_x = 0
+moon_y = 20
 # -- Manages how fast screen refreshes 
 clock = pygame.time.Clock()
 
@@ -39,22 +44,46 @@ while not done:
         #End If
     #Next event
     # -- Game logic goes after this comment
-    # -- Screen background is BLACK 
-    screen.fill (BLACK) 
 
-    # -- Draw Orange Giant (sun)
-    pygame.draw.circle(screen, ORANGE, (sun_x, sun_y),200)
     
-    #retrieve sun
-    if sun_x != screen_x + 200:
-        sun_x = sun_x + 2
-        sun_y = 1/1024*(sun_x - 512)**2 + 200
-    elif sun_x == screen_x + 200:
-        sun_x = -200
+
+
+    sun_round = True
+    moon_round = False
+
+    if sun_round == True:
+        #retrieve sun
+        if sun_x != screen_x + 100:
+            screen.fill (LIGHTBLUE)
+            # -- Draw Orange Giant (sun)
+            pygame.draw.circle(screen, ORANGE, (sun_x, sun_y),200)
+            sun_x = sun_x + 2
+            sun_y = 1/1024*(sun_x - 512)**2 + 200
+        elif sun_x == screen_x + 100:
+            sun_x == -100
+            moon_round = True
+            sun_round = False
+        #endif
     #endif
 
+    if moon_round == True:
+        #get moon
+        if moon_x != screen_x + 18:
+            screen.fill(DARKBLUE)
+            #draw moon
+            pygame.draw.circle(screen, MOONWHITE, (moon_x, moon_y),20)
+            moon_x = moon_x + 2
+            moon_y = 1/1024*(moon_x - 512)**2 + 200
+        elif moon_x == screen_x + 18:
+            moon_x == -20
+            sun_round == True
+            moon_round == False
+        #endif
+    #endif
+
+
     # -- Draw mansion
-    pygame.draw.rect(screen, LIGHTBLUE, (362,350,300,150))
+    pygame.draw.rect(screen, OAK, (362,350,300,150))
     # mansion windows
     pygame.draw.rect(screen, WHITE, (382, 360, 60, 50))
     pygame.draw.rect(screen, WHITE, (382, 435, 60, 50))
