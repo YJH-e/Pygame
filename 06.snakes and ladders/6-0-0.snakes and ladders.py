178cf54c1e05e4806075eafe9c3eaeab5c114541
YJH-e <85558095+YJH-e@users.noreply.github.com>
2021-11-23 15:19:18 +0000

    start with s and l
    s and l is short for snakes and ladders

---
+++ 06.snakes and ladders/6-0-0.snakes and ladders.py
@@ -0,0 +1,49 @@
+#game specification
+# - 10 by 10 board, 100 spaces
+# - 5 snakes
+# - 5 ladders
+# - 2 players
+# - to start, players are off the board with first step on space no.1
+# - to win, players must land exactly on space no.100. additional steps will bounce back
+
+
+
+
+#class obstacles
+from _typeshed import StrPath
+
+
+class Obstacle:
+    start =
+    end =
+    #ascending numbers as positive direction (e.g. 10 to 50)
+    direction = 
+#end class
+
+
+#class snakes
+class Snakes(Obstacle):
+
+#end class
+
+
+#class ladders
+class Ladders(Obstacle):
+
+#end class
+
+#class player
+class Player:
+
+#end class
+
+
+#class board
+class Board:
+
+#end class
+
+
+##############################################           Game Loop           ##############################################
+
+#end game loop
